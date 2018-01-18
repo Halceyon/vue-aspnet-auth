affb3ac7a7a8e97f16381d9ee094de34d8420c13
Craig Pretorius <craigp@codehq.co.za>
2018-01-18 18:28:38 +0200

    Revert "Fix IE11 bug"
    This reverts commit a6c02184030746ad06539305cb36d467d8ea6ecb.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import aspnetAuth from './aspnetAuth';
 export const AspnetAuth = {
-  install: function (Vue) {
+  install(Vue) {
     Vue.prototype.$auth = aspnetAuth;
     window.aspnetAuth = aspnetAuth;
   }
