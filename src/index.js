a6bdeae6342b80b7fc1ee385efb314d42ff06c29
Craig Pretorius <craigp@codehq.co.za>
2018-02-23 18:04:20 +0200

    Added unit tests

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@
  * Copyright © 2018 Code HQ (Pty) Ltd. All rights reserved.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
+ * LICENSE file in the root directory of this source tree.
  */
 
-import AspnetAuth from 'aspnet-auth';
+import aspnetAuth from 'aspnet-auth';
 export const AspnetAuth = {
   install(Vue) {
-    Vue.prototype.$auth = AspnetAuth;
+    Vue.prototype.$auth = new aspnetAuth({
+      url: 'http://localhost'
+    });
   }
 }
---
+++ test/vue-aspnet-auth-Spec.js
@@ -0,0 +1,44 @@
+/**
+ * vue-aspnet-auth (https://github.com/Halceyon/vue-aspnet-auth)
+ *
+ * Copyright © 2018 Code HQ (Pty) Ltd. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Vue from 'vue';
+import { AspnetAuth } from '../src/index';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import cookies from 'browser-cookies';
+
+describe('vue-aspnet-auth', () => {
+
+  before(() => {
+    sinon.stub(cookies, 'get').returns(null);
+    Vue.use(AspnetAuth);
+  });
+  after(() => {
+    cookies.get.restore();
+  });
+
+  it('attached itself to vue as $auth', () => {
+    expect(Vue.prototype.$auth).to.be.an('object');
+  });
+  it('has a login method', () => {
+    expect(Vue.prototype.$auth.login).to.be.a('function');
+  });
+  it('has a logout method', () => {
+    expect(Vue.prototype.$auth.logout).to.be.a('function');
+  });
+  it('has a loginExternal method', () => {
+    expect(Vue.prototype.$auth.loginExternal).to.be.a('function');
+  });
+  it('has a register method', () => {
+    expect(Vue.prototype.$auth.register).to.be.a('function');
+  });
+  it('has a forgot method', () => {
+    expect(Vue.prototype.$auth.forgot).to.be.a('function');
+  });
+});
