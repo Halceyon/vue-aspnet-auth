47e1ea0bc6bf9910cf8a080d2164fece1bd04c82
Craig Pretorius <craigp@codehq.co.za>
2019-05-13 04:47:31 +0200

    fix(dependency): updated aspnet-auth

--- dist/vue-aspnet-auth.umd.js
+++ dist/vue-aspnet-auth.umd.js
@@ -1,28 +1,28 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('aspnet-auth')) :
-	typeof define === 'function' && define.amd ? define(['exports', 'aspnet-auth'], factory) :
-	(factory((global['vue-aspnet-auth'] = {}),global.aspnetAuth));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('aspnet-auth')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'aspnet-auth'], factory) :
+  (factory((global['vue-aspnet-auth'] = {}),global.aspnetAuth));
 }(this, (function (exports,aspnetAuth) { 'use strict';
 
-aspnetAuth = aspnetAuth && aspnetAuth.hasOwnProperty('default') ? aspnetAuth['default'] : aspnetAuth;
+  aspnetAuth = aspnetAuth && aspnetAuth.hasOwnProperty('default') ? aspnetAuth['default'] : aspnetAuth;
 
-/**
- * vue-aspnet-auth (https://github.com/Halceyon/vue-aspnet-auth)
- *
- * Copyright © 2018 Code HQ (Pty) Ltd. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-var AspnetAuth = {
-  install: function install(Vue, options) {
-    Vue.prototype.$auth = new aspnetAuth(options);
-  }
-};
+  /**
+   * vue-aspnet-auth (https://github.com/Halceyon/vue-aspnet-auth)
+   *
+   * Copyright © 2018 Code HQ (Pty) Ltd. All rights reserved.
+   *
+   * This source code is licensed under the MIT license found in the
+   * LICENSE file in the root directory of this source tree.
+   */
+  var AspnetAuth = {
+    install: function install(Vue, options) {
+      Vue.prototype.$auth = new aspnetAuth(options);
+    }
+  };
 
-exports.AspnetAuth = AspnetAuth;
+  exports.AspnetAuth = AspnetAuth;
 
-Object.defineProperty(exports, '__esModule', { value: true });
+  Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
 //# sourceMappingURL=vue-aspnet-auth.umd.js.map
